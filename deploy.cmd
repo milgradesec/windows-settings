d93f84e1f34c07a769e89a9adf48234469c1949b
milgradesec <38557680+milgradesec@users.noreply.github.com>
2020-08-04 15:51:30 +0200

    Deploy to new path

--- deploy.cmd
+++ deploy.cmd
@@ -1,1 +1,2 @@
 scp windows10.ps1 ubuntu@192.168.1.100:/home/ubuntu/www/dlserver/krypton/configurarWindows10.ps1
+scp windows10.ps1 ubuntu@192.168.1.100:/home/ubuntu/www/dlserver/windows/windows10.ps1
