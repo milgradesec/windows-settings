fbce63581a9a7c0f4eb865a46c23d10f146be735
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-01-08 20:56:33 +0100

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,2 +1,1 @@
-scp windows10.ps1 ubuntu@192.168.1.100:/home/ubuntu/www/dlserver/krypton/configurarWindows10.ps1
-scp windows10.ps1 ubuntu@192.168.1.100:/home/ubuntu/www/dlserver/windows/windows10.ps1
+scp windows10.ps1 ubuntu@ssh.paesa.es:/home/ubuntu/www/dlserver/krypton/configurarWindows10.ps1
