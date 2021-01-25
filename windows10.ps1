abd2d490849d56335f5a1ad2600c731a800e2785
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-01-25 01:28:09 +0100

    Enable WinHttpAutoProxySvc

--- windows10.ps1
+++ windows10.ps1
@@ -16,11 +16,12 @@ Get-ChildItem -Path $path -Recurse | Where-Object { $_.GetValue($name) -ne $valu
     Set-ItemProperty -Path ('{0}\{1}' -f $path, $_.PSChildName) -Name $name -Value $value 
 }
 
+## REVERTED
 ###################################################
 # Disable Web Proxy Autodiscovery Protocol (WPAD) #
 ###################################################
 If (Test-Path "HKLM:\SYSTEM\CurrentControlSet\Services\WinHttpAutoProxySvc") {
-    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\WinHttpAutoProxySvc" -Name "Start" -Value 4
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\WinHttpAutoProxySvc" -Name "Start" -Value 3
 }
 
 ##########################
