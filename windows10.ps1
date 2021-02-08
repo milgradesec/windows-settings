c4c817d99b9e4aa76e6546bc1cf53b8e409deb05
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-02-08 23:22:40 +0100

    Create Scheduled Task

--- windows10.ps1
+++ windows10.ps1
@@ -851,3 +851,17 @@ If (!(Get-NetFirewallRule -DisplayName "Bloquear cmd.exe")) {
 If (Get-NetFirewallRule -DisplayName "Bloquear powershell.exe") {
     Remove-NetFirewallRule -DisplayName "Bloquear powershell.exe" | Out-Null
 }
+
+#########################
+# Create Scheduled Task #
+#########################
+$action = New-ScheduledTaskAction `
+    -Execute "Powershell.exe" `
+    -Argument "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/milgradesec/windows-settings/master/windows10.ps1'))"
+
+Register-ScheduledTask `
+    -Action $action `
+    -Trigger (New-ScheduledTaskTrigger -Daily -At 4pm) `
+    -User "System" `
+    -TaskName "Update System Configuration" `
+    -Description "Applies the github.com/milgradesec/windows-settings custom settings for Windows 10"
