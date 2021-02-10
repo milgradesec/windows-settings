57260a76fb56bc70f34b9eba3a3518adc44375d6
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-02-10 01:09:14 +0100

    Update windows10.ps1

--- windows10.ps1
+++ windows10.ps1
@@ -860,8 +860,10 @@ $action = New-ScheduledTaskAction `
     -Argument "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/milgradesec/windows-settings/master/windows10.ps1'))"
 
 Register-ScheduledTask `
+    -Force `
     -Action $action `
     -Trigger (New-ScheduledTaskTrigger -Daily -At 4pm) `
+    -Settings (New-ScheduledTaskSettingsSet -StartWhenAvailable -ExecutionTimeLimit (New-TimeSpan -Hours 1)) `
     -User "System" `
     -TaskName "Update System Configuration" `
     -Description "Applies the github.com/milgradesec/windows-settings custom settings for Windows 10"
