ce13d7281ef2d938a1fe585dfc19abd419cd02ac
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-07-13 11:26:45 +0200

    Enable SmartScreen

--- windows10.ps1
+++ windows10.ps1
@@ -520,6 +520,9 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "BlockThir
 # Block popups
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "DefaultPopupsSetting" -Value 2
 
+# Enable SmartScreen
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\PhishingFilter" -Name "EnabledV9" -Value 1
+
 # Block insecure content
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "DefaultInsecureContentSetting" -Value 2
 
