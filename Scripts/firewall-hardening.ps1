84717831f2db0f4b70f53ee9e347fd26956f1509
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-10-29 11:03:17 +0200

    Update firewall-hardening.ps1

--- Scripts/firewall-hardening.ps1
+++ Scripts/firewall-hardening.ps1
@@ -1,14 +1,14 @@
 New-NetFirewallRule `
     -DisplayName "Block Outbound HTTP" `
     -Direction Outbound `
-    -LocalPort 80 `
+    -RemotePort 80 `
     -Protocol TCP `
     -Action Block
 
 New-NetFirewallRule `
     -DisplayName "Allow Outbound HTTP on LocalSubnet" `
     -Direction Outbound `
-    -LocalPort 80 `
+    -RemotePort 80 `
     -Protocol TCP  `
     -RemoteAddress LocalSubnet `
     -Action Allow
