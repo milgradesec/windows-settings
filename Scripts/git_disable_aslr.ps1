d5bdfcdc8efc8a8363c319ee960f217eefe706ea
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-11-05 16:49:50 +0100

    Update git_disable_aslr.ps1

--- Scripts/git_disable_aslr.ps1
+++ Scripts/git_disable_aslr.ps1
@@ -1,1 +1,3 @@
+Get-Item -Path "C:\Program Files\Git\bin\*.exe" | ForEach-Object { Set-ProcessMitigation -Name $_.Name -Disable ForceRelocateImages }
+Get-Item -Path "C:\Program Files\Git\mingw64\libexec\git-core\*.exe" | ForEach-Object { Set-ProcessMitigation -Name $_.Name -Disable ForceRelocateImages }
 Get-Item -Path "C:\Program Files\Git\mingw64\bin\*.exe" | ForEach-Object { Set-ProcessMitigation -Name $_.Name -Disable ForceRelocateImages }
