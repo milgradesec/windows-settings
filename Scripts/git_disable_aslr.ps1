6070bc14e05aa1f4f4232ea3a0676068408a0b1e
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-11-23 12:37:34 +0100

    Update git_disable_aslr.ps1

--- Scripts/git_disable_aslr.ps1
+++ Scripts/git_disable_aslr.ps1
@@ -1,3 +1,7 @@
 Get-Item -Path "C:\Program Files\Git\bin\*.exe" | ForEach-Object { Set-ProcessMitigation -Name $_.Name -Disable ForceRelocateImages }
 Get-Item -Path "C:\Program Files\Git\mingw64\libexec\git-core\*.exe" | ForEach-Object { Set-ProcessMitigation -Name $_.Name -Disable ForceRelocateImages }
 Get-Item -Path "C:\Program Files\Git\mingw64\bin\*.exe" | ForEach-Object { Set-ProcessMitigation -Name $_.Name -Disable ForceRelocateImages }
+
+# Get-Item -Path "C:\Program Files\Git\bin\*.exe" | ForEach-Object { Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\$($_.Name)" }
+# Get-Item -Path "C:\Program Files\Git\mingw64\libexec\git-core\*.exe" | ForEach-Object { Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\$($_.Name)" }
+# Get-Item -Path "C:\Program Files\Git\mingw64\bin\*.exe" | ForEach-Object { Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\$($_.Name)" }
