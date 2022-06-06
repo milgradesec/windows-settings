dd3455b0e984e38a6ccd667f08a9d50b06e284bb
milgradesec <38557680+milgradesec@users.noreply.github.com>
2022-06-06 06:22:59 +0200

    Update git-disable-aslr.ps1

--- Scripts/git-disable-aslr.ps1
+++ Scripts/git-disable-aslr.ps1
@@ -1,6 +1,25 @@
-Get-Item -Path "C:\Program Files\Git\bin\*.exe" | ForEach-Object { Set-ProcessMitigation -Name $_.Name -Disable ForceRelocateImages }
-Get-Item -Path "C:\Program Files\Git\mingw64\libexec\git-core\*.exe" | ForEach-Object { Set-ProcessMitigation -Name $_.Name -Disable ForceRelocateImages }
-Get-Item -Path "C:\Program Files\Git\mingw64\bin\*.exe" | ForEach-Object { Set-ProcessMitigation -Name $_.Name -Disable ForceRelocateImages }
+
+function Disable-ASLR {
+    Get-Item -Path "C:\Program Files\Git\bin\*.exe" | ForEach-Object { 
+        Set-ProcessMitigation -Name $_.Name -Disable ForceRelocateImages
+        Write-Output "Disabled ASLR for $_"
+    }
+    
+    Get-Item -Path "C:\Program Files\Git\usr\bin\*exe" | ForEach-Object { 
+        Set-ProcessMitigation -Name $_.Name -Disable ForceRelocateImages
+        Write-Output "Disabled ASLR for $_"
+    }
+    
+    Get-Item -Path "C:\Program Files\Git\mingw64\bin\*.exe" | ForEach-Object { 
+        Set-ProcessMitigation -Name $_.Name -Disable ForceRelocateImages
+        Write-Output "Disabled ASLR for $_"
+    }
+    
+    Get-Item -Path "C:\Program Files\Git\mingw64\libexec\git-core\*.exe" | ForEach-Object {
+        Set-ProcessMitigation -Name $_.Name -Disable ForceRelocateImages
+        Write-Output "Disabled ASLR for $_"
+    }
+}
 
 # Get-Item -Path "C:\Program Files\Git\bin\*.exe" | ForEach-Object { Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\$($_.Name)" }
 # Get-Item -Path "C:\Program Files\Git\mingw64\libexec\git-core\*.exe" | ForEach-Object { Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\$($_.Name)" }
