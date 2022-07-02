d4e1fd9f62609d4ad89efdd92c9b276fa9259e4f
milgradesec <38557680+milgradesec@users.noreply.github.com>
2022-07-03 00:54:02 +0200

    Ensure WD Cloud protection is enabled

--- windows10.ps1
+++ windows10.ps1
@@ -837,6 +837,7 @@ Get-ScheduledTask -TaskName "NvTmRep_CrashReport*" | Disable-ScheduledTask
 Write-Output "Configuring Windows Defender..."
 Set-MpPreference -PUAProtection Enabled
 Set-MpPreference -CloudBlockLevel Default
+Set-MpPreference -MAPSReporting Advanced
 Set-MpPreference -SubmitSamplesConsent SendSafeSamples
 Set-MpPreference -CloudExtendedTimeout 50
 Set-MpPreference -SignatureUpdateInterval 8
